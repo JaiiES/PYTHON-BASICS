ea5332423574b895984b885c2676ee8405b95ccc
JaiiES <priyadharshini.civil@gmail.com>
2024-07-21 17:15:55 +0530

    Add files via upload

---
+++ project 1.py
@@ -0,0 +1,32 @@
+#Traffic light simulator
+light=str(input("enter the light here: "))
+if(light=="red"):
+    print("red means stop")
+elif(light=="green"):
+   print("green means go")
+elif(light=="yellow"):
+    print("yellow means wait")
+else:
+    print("invalid colour")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
